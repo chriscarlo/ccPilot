4e0b03ee555e833bdd5791ace45758b8d3bc5397
chriscarlo <150490785+chriscarlo@users.noreply.github.com>
2024-03-15 21:55:43 -0700

    Update safety_hyundai.h

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -12,22 +12,22 @@
    /* the EPS faults when the steering angle is above a certain threshold for too long. to prevent this, */ \
    /* we allow setting CF_Lkas_ActToi bit to 0 while maintaining the requested torque value for two consecutive frames */ \
   .min_valid_request_frames = 89, \
-  .max_invalid_request_frames = 2, \
+  .max_invalid_request_frames = 4, \
   .min_valid_request_rt_interval = 810000,  /* 810ms; a ~10% buffer on cutting every 90 frames */ \
   .has_steer_req_tolerance = true, \
 }
 
-const SteeringLimits HYUNDAI_STEERING_LIMITS = HYUNDAI_LIMITS(384, 3, 7);
+const SteeringLimits HYUNDAI_STEERING_LIMITS = HYUNDAI_LIMITS(520, 3, 7);
 const SteeringLimits HYUNDAI_STEERING_LIMITS_ALT = HYUNDAI_LIMITS(270, 2, 3);
 
 const LongitudinalLimits HYUNDAI_LONG_LIMITS = {
-  .max_accel = 200,   // 1/100 m/s2
-  .min_accel = -350,  // 1/100 m/s2
+  .max_accel = 400,   // 1/100 m/s2
+  .min_accel = -400,  // 1/100 m/s2
 };
 
 const LongitudinalLimits HYUNDAI_LONG_LIMITS_SPORT = {
-  .max_accel = 400,   // 1/100 m/s2
-  .min_accel = -350,  // 1/100 m/s2
+  .max_accel = 600,   // 1/100 m/s2
+  .min_accel = -600,  // 1/100 m/s2
 };
 
 const CanMsg HYUNDAI_TX_MSGS[] = {
